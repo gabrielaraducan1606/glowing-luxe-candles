082f3c55f7593c51e4e6baa01cc5ce668fb74f16
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2026-02-19 16:08:14 +0200

    commit4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header/Header.jsx";
-import Home from "./pages/Home/Home";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import CategoryPage from "./pages/CategoryPage/CategoryPage";
+import Home from "./pages/Home/Home.jsx";
+import Services from "./pages/Services.jsx";
+import Contact from "./pages/Contact.jsx";
+import CategoryPage from "./pages/CategoryPage/CategoryPage.jsx";
 import "./styles.css";
 
 export default function App() {
